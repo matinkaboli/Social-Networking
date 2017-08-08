56225f9d0f6e3c47254b3192f14bf38d69487745
matinkaboli <matinkaboli@aol.com>
2017-08-08 15:36:22 +0430

    Contact us

---
+++ public/script/contact.js
@@ -0,0 +1,37 @@
+const send = document.getElementById("send");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const content = document.getElementById("content");
+
+send.addEventListener("click", () => {
+  const configuration = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name.value,
+      email: email.value,
+      content: content.value
+    })
+  };
+  sendEmail("/contact", configuration);
+});
+function sendEmail(url, configuration) {
+  fetch(url, configuration)
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+function checkStatus(res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else {
+    const error = new Error(res.statusText);
+    error.res = res;
+    throw error;
+  }
+}
