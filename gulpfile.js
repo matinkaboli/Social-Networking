c039e4ca7ef367a991a37fcfaf3ec9f8a7bff34a
matinkaboli <matinkaboli@aol.com>
2017-09-06 01:23:38 +0430

    Fixing Bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,48 @@
-const gulp = require("gulp");
-const cleanCSS = require("gulp-clean-css");
-const replace = require("gulp-replace");
-const htmlmin = require("gulp-htmlmin");
-const uglify = require('gulp-uglify-es').default;
+var gulp = require("gulp");
+var cleanCSS = require("gulp-clean-css");
+var replace = require("gulp-replace");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require('gulp-uglify-es').default;
+var babel = require("gulp-babel");
 
-const config = require("./config.json");
+var config = require("./config.json");
 
 gulp.task("default", ["watch"]);
 
-gulp.task("miniC", () => {
+gulp.task("miniC", function() {
   return gulp.src("./st/*.css")
     .pipe(cleanCSS())
     .pipe(gulp.dest("./public/styles"));
 });
-gulp.task("miniH", () => {
+gulp.task("miniH", function() {
   return gulp.src("view/*.njk")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("views"));
 });
-gulp.task("rep1", () => {
+gulp.task("rep1", function() {
   return gulp.src("./view/*.njk")
     .pipe(replace(/rootpath/g, config.website))
     .pipe(gulp.dest("./view"));
 });
-gulp.task("rep2", () => {
+gulp.task("rep2", function() {
   return gulp.src("./server/*.js")
     .pipe(replace(/rootpath/g, config.website))
     .pipe(gulp.dest("./server"));
 });
-gulp.task("rep3", () => {
+gulp.task("rep3", function() {
   return gulp.src("./server/*.js")
     .pipe(replace(/maindir/g, config.dir))
     .pipe(gulp.dest("./server"));
 });
-gulp.task("miniJ", ["miniC", "miniH", "rep1", "rep2", "rep3"], () => {
+gulp.task("miniJ", function() {
   return gulp.src("./sc/*.js")
+    .pipe(babel({
+      presets: ['env']
+    }))
     .pipe(uglify())
     .pipe(gulp.dest("./public/script"));
 });
 
-gulp.task("watch", () => {
+gulp.task("watch", function() {
   gulp.watch("./sc/*.js", ["miniJ"]);
 });
